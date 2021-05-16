ff3883e104d703000388ad15e272bd0d2fcfad86
kaue <kauemsc@gmail.com>
2021-05-16 16:30:47 -0300

    lumen structure

---
+++ lumen/app/Application/Console/Kernel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Application\Console;
+
+use App\Application\Console\Commands\CreateTransaction;
+use Illuminate\Console\Scheduling\Schedule;
+use Laravel\Lumen\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        CreateTransaction::class
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        //
+    }
+}
